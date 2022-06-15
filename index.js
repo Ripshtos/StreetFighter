1a3436f1bcc4559bea3308c024d0afc05e86d4d6
Ripshtos <105322785+Ripshtos@users.noreply.github.com>
2022-06-15 08:55:11 +0300

    fixed the game back to when the game used to work added hitboxes

--- index.js
+++ index.js
@@ -1,143 +1,150 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillRect(0 ,0 , canvas.width, canvas.height);
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.7;
+const gravity = 0.2;
 
 class Sprite {
-    constructor({position,velocity})
-    {
-        this.position = position; 
+    constructor({ position, velocity }) {
+        this.position = position;
         this.velocity = velocity;
         this.height = 150;
         this.lastkey;
+        this.width = 50;
         this.hitbox = { 
             position : this.position,
             width : 100,
             height : 50
         }
     }
-    
-    draw(){
+
+    draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x,this.position.y, 50 , this.height);
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
         c.fillStyle = 'green';
         c.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height);
     }
-    
-    update(){
+
+    update() {
         this.draw()
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-
-        if(this.position.y + this.height + this.velocity.y >= canvas.height){ // set floor
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) { // set floor
             this.velocity.y = 0;
         }
-        else {this.velocity.y += gravity;} 
-        if(this.position.x + this.velocity.x < 5 ) { // set border right
+        else { this.velocity.y += gravity; }
+
+        if (this.position.x + this.velocity.x < 5) { // set border right
             this.position.x = 5;
         }
-      
-        if(this.position.x + this.velocity.x > 970){ // set border left
+
+        if (this.position.x + this.velocity.x > 970) { // set border left
             this.position.x = 970;
-        }  
-        
-        if( this.position.y + this.velocity.y + this.height <= 100) // set border up
+        }
+
+        if (this.position.y + this.velocity.y + this.height <= 0) // set border up
         {
-            this.position.y = 100;
-        } 
-    } 
+            this.position.y = 50;
+        }
+    }
 }
 
 
 
 
 const player = new Sprite({ //player character
-    position :{
-    x:10,
-    y:0},
-    velocity : {
-    x : 0,
-    y : 10
-    }});
+    position: {
+        x: 10,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
+});
 
 const enemy = new Sprite({ //enemy character
-    position :{
-    x:600,
-    y:0},
-    velocity : {
-    x : 0,
-    y : 10
-    }});
+    position: {
+        x: 600,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
+});
 
 
 const keys = { //constant that contains the if current key is pressed 
-a: { pressed : false} ,
-d: { pressed : false } 
+    a: { pressed: false },
+    d: { pressed: false },
+    w: { pressed: false }
 }
-let lastkey = 'd';
+
+let lastkey = 'z';
 
 
 
-function animate() { //animation loop
-    window.requestAnimationFrame(animate);  
+function animate() //animation loop
+{
     c.fillStyle = 'black';
-    c.fillRect(0,0,canvas.width,canvas.height);
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+    if (keys.d.pressed && lastkey === 'd') { player.velocity.x = 3; }
+    else if (keys.a.pressed && lastkey === 'a') { player.velocity.x = -3; }
+    else {
+
+    }
+
     player.update();
     enemy.update();
+    window.requestAnimationFrame(animate);
 
-    if( keys.d.pressed && lastkey === 'd' )
-    { player.velocity.x = 3;}
-    else if( keys.a.pressed && lastkey === 'a' )
-    { player.velocity.x = -3;}  
-    
-    if( player.hitbox.position.x + player.hitbox.width >= enemy.position.x && player.hitbox.position.x <= enemy.hitbox.position.x + enemy.hitbox.width)
-    {
-        console.log('go');
-    }
-    
 }
 
 animate();
 
-window.addEventListener('keydown' , (event) => {
-    switch (event.key){
+window.addEventListener('keydown', (event) => {
+    console.log(event);
+    switch (event.key) {
         case 'd':
             keys.d.pressed = true;
-            lastkey ='d';
+            lastkey = 'd';
             break;
-        
+
         case 'a':
             keys.a.pressed = true;
-            lastkey ='a';
+            lastkey = 'a';
             break;
 
         case 'w':
-            player.velocity.y = -10;
-            
+            keys.w.pressed = true;
+            player.velocity.y = -2;
+
             break;
     }
 });
 
-window.addEventListener('keyup' , (event) => { 
-
-    switch (event.key){
+window.addEventListener('keyup', (event) => {
+    console.log(event);
+    switch (event.key) {
         case 'd':
             keys.d.pressed = false;
             lastkey = 'd';
             break;
-        
+
         case 'a':
             keys.a.pressed = false;
-            lastkey ='a';
+            lastkey = 'a';
             break;
 
         case 'w':
-            lastkey ='w';
+            keys.w.pressed = true;
+            lastkey = 'w';
             break;
     }
 });
